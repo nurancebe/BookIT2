3b3bf076f3003f60db1d9edcbb240d8f1e5a970e
nurancebe <nurancebe@gmail.com>
2020-02-06 16:21:23 -0500

    heyooo

---
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -0,0 +1,18 @@
+package com.bookit.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+          "json:target/cucumber.json"
+        },
+        features = "src/test/resources/features",
+        glue = "com/bookit/step_definitions",
+        dryRun = true,
+        tags = "@api and @create_student"
+)
+public class CucumberRunner {
+}
